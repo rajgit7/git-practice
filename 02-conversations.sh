6ae2ce400ad7414a99afb4e5af121d01f05924c2
rajgit7 <rajprudvin@gmail.com>
2025-03-06 18:27:26 +0530

    practice shell

---
+++ 02-conversations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh::hi suresh. How are you?"
+echo "Suresh::hello Ramesh iam fine.how are you doing?"
+echo "Ramesh::im good Suresh what is going on?"
+echo "suresh::i started practice shell script Ramesh"
