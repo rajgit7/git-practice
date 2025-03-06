11fe95c1ae9ea7c991c45b87a66aed458bd52fe5
rajgit7 <rajprudvin@gmail.com>
2025-03-06 18:38:03 +0530

    adding variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1::hi $PERSON2. how are you?"
+echo "$PERSON2::hello $PERSON1 iam fine.how are you doing?"
+echo "$PERSON1::im good $PERSON2 what is going on?"
+echo "$PERSON2::i started practice shell script $PERSON1"
---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::hi $PERSON2. how are you?"
+echo "$PERSON2::hello $PERSON1 iam fine.how are you doing?"
+echo "$PERSON1::im good $PERSON2 what is going on?"
+echo "$PERSON2::i started practice shell script $PERSON1"
